2840788766dad9c1cff5c906e12cc22b364516ab
unknown <marjolaine38@hotmail.fr>
2021-06-04 11:10:50 +0200

    test8

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -6,11 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RoutesTest extends WebTestCase
 {
-    public function testHomepageIsUp()
+    public function testHomepageIsUp($url)
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $client->request('GET', $url);
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testAllRoutes()
+    {
+        yield['/'];
+        yield['/fr/boutique'];
+        yield['/fr/contact'];
+        yield['/fr/contact'];
+        yield['/fr/rayon/1'];
+        yield['/fr/panier'];
+        yield['/fr/panier'];
     }
 }
