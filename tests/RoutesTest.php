4c4f6eb9b2671a5ce9c66192e2515419ae1f1beb
unknown <marjolaine38@hotmail.fr>
2021-06-08 18:11:10 +0200

    testRoutes

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -3,10 +3,10 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Flex\Path;
 
 class RoutesTest extends WebTestCase
 {
-
     /**
     * @dataProvider urlProvider
      */
@@ -25,5 +25,61 @@ class RoutesTest extends WebTestCase
         yield['/fr/contact'];
         yield['/fr/rayon/1'];
         yield['/fr/panier'];
+        yield['/fr/connexion'];
+        yield['/fr/accueil'];
+        yield['/fr/inscription'];
+    }
+
+    //test '/' redirects to Home
+    public function testCorrectHome() {
+        $this->client = self::createClient();
+        $crawler = $this->client->request('GET', '/');
+        $this->assertSame('Bienvenue sur boublaCar', $crawler->filter('h1'));
+    }
+    //test homepage contient 10 li
+    public function testHomeContainsLi() {
+
+        $this->client = self::createClient();
+        $crawler = $this->client->request('GET', '/');
+        $this->assertCount(10, $crawler->filter('li'));
+    }
+
+    // test li contains Paths
+    public function testHomeContainsPaths() {
+        $this->client = self::createClient();
+        $crawler = $this->client->request('GET', '/');
+        $this->assertContainsOnlyInstancesOf(Path::class,$crawler->filter('li'));
+    }
+
+    public function testContainsRegister() {
+        $this->client = self::createClient();
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertContains('/register',$crawler->filter('a')->extract('href'));
+    }
+
+    public function testContainsLogIn() {
+        $this->client = self::createClient();
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertContains('/login',$crawler->filter('a')->extract('href'));
+    }
+
+    public function testLoginTrue()
+    {
+        $credential = [
+            'email' => 'user@gmail.com',
+            'password' => 'user'
+        ];
+        $this->post('login',$credential)->assertRedirect('/');
+    }
+
+    public function testLoginFalse()
+    {
+        $credential = [
+            'email' => 'usera@gmil.com',
+            'password' => 'usera'
+        ];
+        $this->post('login',$credential)->assertRedirect('/login');
     }
 }
